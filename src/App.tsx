608cfb81f2ebffb22df889c4a8387c10424a46ca
Hidenari Kobayashi <hide1982aug@gmail.com>
2020-12-24 22:47:06 +0900

    feat: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
+
 import Button from "./components/Button";
+import Icon from "./components/Icon";
 
 function App() {
+  const ref = useRef(null);
+  useEffect(() => {
+    if (ref.current) {
+      console.log("App ref ==================");
+      console.log(ref);
+    }
+  }, [ref]);
   return (
     <div className="App">
-      <Button>button</Button>
+      <Button
+        onClick={() => {
+          console.log("hoge");
+        }}
+      >
+        button
+      </Button>
+      <Button>button2</Button>
+      <Button>button3button3button3button3button3</Button>
+      <Icon name="search" width={50} height={50} />
     </div>
   );
 }
