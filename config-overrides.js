cd9faca97d1e944066795e624171898f8b41b607
Hidenari Kobayashi <hide1982aug@gmail.com>
2021-01-10 22:20:12 +0900

    chore: update addBabelPlugin utility

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,12 @@
-const { override, addBabelPlugin } = require("customize-cra");
+const { override, getBabelLoader } = require("customize-cra");
+
+const addBabelPlugin = (plugin, mode) => (config) => {
+  if (mode && config.mode === mode) {
+    getBabelLoader(config).options.plugins.push(plugin);
+  }
+  return config;
+};
 
 module.exports = override(
-  addBabelPlugin("babel-plugin-jsx-remove-data-test-id")
+  addBabelPlugin("babel-plugin-jsx-remove-data-test-id", "production")
 );
