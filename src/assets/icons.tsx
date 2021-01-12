60436c2f942a2a7c6211c686e8bdc57bcaee3119
Hidenari Kobayashi <hide1982aug@gmail.com>
2021-01-12 09:42:01 +0900

    fix: fix icon name

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -6,12 +6,12 @@ const icons = {
       <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
     </svg>
   ),
-  cheveronLeft: (
+  chevronLeft: (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
       <path d="M7.05 9.293L6.343 10 12 15.657l1.414-1.414L9.172 10l4.242-4.243L12 4.343z" />
     </svg>
   ),
-  cheveronRight: (
+  chevronRight: (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
       <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
     </svg>
